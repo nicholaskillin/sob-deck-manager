9c20003509bfdd3fe901a487251cf45a7a2e40bb
Nick Killin <nicholaskillin@gmail.com>
2020-10-04 11:04:22 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import { Divider, StackView } from '@planning-center/ui-kit'
+
+import Deck from './Deck.js'
+import React from 'react'
+
+// Import Deck Data
+// const forbiddenFortressEncounterDeck = require('./deckData/forbiddenFortress/encounters.json')
+const forbiddenFortressMapDeck = require('./deckData/forbiddenFortress/map.json')
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StackView spacing={<Divider margin={2} />}>
+        <Deck deck={forbiddenFortressMapDeck} />
+      </StackView>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Deck.js
@@ -0,0 +1,65 @@
+import { Button, StackView, Text } from '@planning-center/ui-kit'
+import React, { useEffect, useState } from 'react'
+
+import _ from 'lodash'
+
+function Deck({ deck }) {
+  const [activeCard, setActiveCard] = useState()
+  const [localDeck, setLocalDeck] = useState(deck.cards)
+
+  useEffect(() => {
+    setLocalDeck(_.shuffle(localDeck))
+  }, [])
+
+  function drawCard() {
+    setActiveCard(_.sample(localDeck.shift()))
+  }
+
+  function shuffleDeck() {
+    setLocalDeck(_.shuffle(deck.cards))
+    setActiveCard()
+  }
+
+  return (
+    <>
+      <h2>
+        {deck.game} {deck.name}
+      </h2>
+      <StackView distribution="center" axis="vertical" spacing={1}>
+        <StackView distribution="center" axis="horizontal" spacing={2}>
+          {localDeck.length > 0 && (
+            <img
+              alt={`${deck.game} ${deck.name} Card Back`}
+              onClick={drawCard}
+              src={deck.backImageURL}
+            />
+          )}
+          {activeCard && (
+            <img
+              alt={activeCard}
+              src={`/images/${_.camelCase(deck.game)}/${_.camelCase(
+                deck.name
+              )}/${_.camelCase(activeCard)}.png`}
+            />
+          )}
+        </StackView>
+        <StackView
+          alignment="center"
+          distribution="center"
+          axis="horizontal"
+          spacing={2}
+        >
+          <Text>{localDeck.length} left</Text>
+          <Button
+            onClick={shuffleDeck}
+            disabled={localDeck.length === deck.cards.length}
+            fontSize={18}
+            title="Shuffle"
+          />
+        </StackView>
+      </StackView>
+    </>
+  )
+}
+
+export default Deck
