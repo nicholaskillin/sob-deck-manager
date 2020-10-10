1bcd5333e4683bb3ed761e0da6014cbe7f6622ad
Nick Killin <nicholaskillin@gmail.com>
2020-10-10 17:02:11 -0400

    add threat level high deck data

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ let decks = [
   {data: require('./deckData/forbiddenFortress/loot.json'), orientation: 'portrait' },
   {data: require('./deckData/forbiddenFortress/map.json'), orientation: 'portrait' },
   {data: require('./deckData/forbiddenFortress/threatLow.json'), orientation: 'portrait'},
-  {data: require('./deckData/forbiddenFortress/threatMedium.json'), orientation: 'portrait'}
+  {data: require('./deckData/forbiddenFortress/threatMedium.json'), orientation: 'portrait'},
+  {data: require('./deckData/forbiddenFortress/threatHigh.json'), orientation: 'portrait'}
 ]
 
 function App() {
